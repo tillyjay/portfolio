b3efc31febc744f0cddc57d383a42f837a7c5327
tillyJay <tilly.jay.campbell@gmail.com>
2024-03-28 11:37:50 -0300

    - created nav bar animation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,49 +1,50 @@
 import React, { useState } from 'react';
 import { Link } from 'gatsby';
+
 import '../styles/componentStyles/navigation.scss';
 
+
 const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setMenuOpen(!menuOpen);
-    };
-  
-    return (
-        <div className={`navigation ${menuOpen ? 'open' : ''}`}>
-        {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <div className="hamburger">
-            <span className="line"></span>
-            <span className="line"></span>
-            <span className="line"></span>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+      <div className={`navigation ${menuOpen ? 'open' : ''}`}>
+      {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className={`box ${menuOpen ? 'active' : 'not-active'}`}>
+          <div className="btn">
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-          <div className="close-icon">
-            <span className="line"></span>
-            <span className="line"></span>
-          </div>
-        </div>
-        <div className="menu">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">Work</Link>
-            </li>
-            <li>
-              <Link to="/services">Resume</Link>
-            </li>
-            <li>
-              <Link to="/contact">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li> 
-          </ul>
         </div>
       </div>
-    );
-  };
+      
+      <div className="menu">
+        <ul>
+          <li>
+            <Link to="/index">Home</Link>
+          </li>
+          <li>
+            <Link to="/portfolioexamples">Work</Link>
+          </li>
+          <li>
+            <Link to="/skills">Resume</Link>
+          </li>
+          <li>
+            <Link to="/contact">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li> 
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
