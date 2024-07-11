2da6bcf764a5520d1795d5225fb846626d24b899
tillyJay <tilly.jay.campbell@gmail.com>
2024-07-11 11:54:58 -0400

    - updated browser icon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,10 @@ require ('bootstrap/dist/css/bootstrap.min.css');
 const Layout = require("./src/components/app/Layout").default
 const React = require("react")
 
+// browser icon
+document.head.appendChild(document.createElement('link'));
+document.querySelector('link').setAttribute('rel', 'shortcut icon');
+document.querySelector('link').setAttribute('href', '/src/images/icon.png');
 
 exports.wrapPageElement = ({element, props}) => {
     return <Layout {...props}>{element}</Layout>
