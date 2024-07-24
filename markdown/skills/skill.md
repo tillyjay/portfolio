a831616b6b0c29a19b0b663450de14add4888f8e
tillyJay <tilly.jay.campbell@gmail.com>
2024-07-24 15:14:36 -0400

    - new component skill triangle

---
+++ markdown/skills/skill.md
@@ -0,0 +1,5 @@
+---
+title: "JavaScript" 
+time: "2.5 Years"
+skillLevel: "Novice to Intermediate"
+---
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,6 +1,7 @@
-import * as React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-
+import Triangle from "../components/Triangle";
 
 const SkillsPage = () => {
   return (
