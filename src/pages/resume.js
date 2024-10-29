45a752c2bcc4657b14229efd0cb32faa9fe75d8a
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-29 14:16:18 -0300

    - feat: added resume blurb component to resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import ResumeBlurb from "../components/ResumeBlurb";
+
 import Triangle from "../components/Triangle";
 
-const ResumePage = () => {
+const ResumePage = ({ data }) => {
+
+  const resume = data; 
+  const title = data.markdownRemark.frontmatter.title; 
+  const html = data.markdownRemark.html;
+
   return (
-    <main>
-  
+    <main className="resume-main container">
+      <ResumeBlurb title={title} blurb={html} />
 
     </main>
-  )
-}
+  );
+};
 
 
+export const resumePageQuery = graphql`
+  query ResumePageQuery {
+    markdownRemark(fileAbsolutePath: { regex: "/pages/resumeBlurb.md/" }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`;
 
 export const Head = () => (
   <>
